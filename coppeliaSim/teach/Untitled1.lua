42225c18433e11033e1a4d7bb88a4adc33698bda
Blinhy0131 <lin.hybrian@gmail.com>
2021-06-13 15:44:04 +0800

    file organize

--- coppeliaSim/teach/Untitled1.lua
+++
@@ -1,6 +0,0 @@
-function sysCall_threadmain()
-    joint01=sim.getObjectHandle('joint1')
-    joint02=sim.getObjectHandle('joint2')
-    joint03=sim.getObjectHandle('joint3')
-    jointz=sim.getObjectHandle('jointz')
-end
