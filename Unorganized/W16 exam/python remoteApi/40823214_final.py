c5d803a8e0896f7ef00d335c419be12d63e814c6
Blinhy0131 <lin.hybrian@gmail.com>
2021-06-14 21:31:48 +0800

    更改內容

--- Unorganized/W16 exam/python remoteApi/40823214_final.py
+++ Unorganized/W16 exam/python remoteApi/40823214_final.py
@@ -45,7 +45,6 @@ clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
   
 if clientID != -1:
     print ('Connected to remote API server')
-      
     res = vrep.simxAddStatusbarMessage(
         clientID, "This is teach by 40823214 ",
         vrep.simx_opmode_oneshot)
